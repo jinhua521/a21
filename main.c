4a0a090f59f766cc37e675f3ef1aaae028225230
shadiao <1498800286@qq.com>
2019-01-18 10:29:02 +0800

    添加学生成绩

--- main.c
+++ main.c
@@ -1,9 +1,43 @@
 #include <stdio.h>
+struct  stru{
+    int yuwei;
+    int shuxue;
+    int yingyu;
+};
 
 int main(){
+    
+    int index = 0;
+    printf("xueshengchenjiguanlixitong\n");
+    while(1){
+        printf("1---tianjiaxueshengchenji\n");
+        printf("2---shanchuxueshengchenji\n");
+        printf("3---suoyouxueshengchenji\n");
+      
+        printf("qingshuxuxuanzedegongnengbianhao\n");
+        int code;
+        scanf("%d",&code);
+      
+        if(code == 1){
+            printf("qingshuruyaotianjiadexueshengchenji :\n");
+            char m;
+            scanf("%c",&m);
+             scanf("%c",&m);
+            
+            index++;
+            printf("tianjiachenggong\n");
+            char x;
+            scanf("%c",&x);
+            scanf("%c",&x);
 
-    printf("hello world!");
-
+          }
+           if(code == 2){
+              
+          }
+           if(code == 3){
+          
+          }
+         
+    }
     return 0;
-
 }
